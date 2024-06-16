0ba7eeebdb375128d9d8e9bd01451ca7135714bb
onnnyxxx <oooivanlau2@gmail.com>
2024-06-16 19:33:58 +0300

    Added: Cache in def index and The new kind

--- coolsite/tasks_app/views.py
+++ coolsite/tasks_app/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render, redirect
 from django.views.generic import DetailView, TemplateView
 from rest_framework import viewsets
 from user_profile.models import Profile
+from django.views.decorators.cache import cache_page
 
 
 # class TopUsersViewSet(viewsets.ReadOnlyModelViewSet):
 #     queryset = Profile.objects.annotate(avg_rating=Avg('comment__stars')).order_by('-avg_rating')
 #     serializer_class = ProfileSerializer
 
+
+@cache_page(10)
 def index(request):
     profiles = Profile.objects.annotate(avg_rating=Avg('comment__stars')).order_by('-avg_rating')
     best_profiles = profiles.filter(avg_rating__gt=3.5)
